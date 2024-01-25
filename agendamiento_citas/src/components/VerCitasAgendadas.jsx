d10e416782b0a737ed2718f2770550949bcdd480
mbsocasi <mbsocasi@espe.edu.ec>
2024-01-25 18:49:08 -0500

    commit

--- agendamiento_citas/src/components/VerCitasAgendadas.jsx
+++ agendamiento_citas/src/components/VerCitasAgendadas.jsx
@@ -6,9 +6,9 @@ function VerCitasAgendadas() {
   const [citas, setCitas] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/appointments') // Asegúrate de reemplazar esto con la URL de tu servidor
+    fetch('http://localhost:3001/appointments')
       .then(response => response.json())
-      .then(data => setCitas(data.appointments));
+      .then(data => setCitas(data));
   }, []);
 
   return (
